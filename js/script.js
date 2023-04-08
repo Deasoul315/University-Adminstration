d3b52fd86fb6ae6f56ba5f8f3362a83ece8441bb
Kyuu <mustafakamel6001@gmail.com>
2023-04-08 06:42:34 -0700

    edit page to be tested

--- js/script.js
+++ js/script.js
@@ -39,6 +39,115 @@ function to_float(x) {
     return y;
 }
 
+function edit() {
+    error = false 
+    
+    let id = document.getElementById("id").value;
+    let name = document.getElementById("name").value;
+    let dob = document.getElementById("dob").value;
+    let gpa = document.getElementById("gpa").value;
+    let lv = document.getElementById("lv").value;
+    let male = document.getElementById("male").checked;
+    let female = document.getElementById("female").checked;
+    let mobile = document.getElementById("mobile").value;
+    let email = document.getElementById("email").value;
+    let active = document.getElementById("active").checked;
+    let inactive = document.getElementById("inactive").checked;
+
+    if (id === "" || id.length > 8 || !(/^[0-9]*$/.test(id))) {
+        document.getElementById("id").style.border = "4px solid #ff3333";
+        document.getElementById("eid").style.display = "inline-block";
+        error = true;
+    } else {
+        document.getElementById("id").style.border = "4px solid rgb(21, 221, 21)";
+        document.getElementById("eid").style.display = "none";
+    }
+
+    if (name === "" || !(/^[A-Za-z ]*$/.test(name))) {
+        document.getElementById("name").style.border = "4px solid #ff3333";
+        document.getElementById("ename").style.display = "inline-block";
+        error = true;
+    } else {
+        document.getElementById("name").style.border = "4px solid rgb(21, 221, 21)";
+        document.getElementById("ename").style.display = "none";
+    }
+
+    if (dob === "" || get_age(dob) < 18) {
+        document.getElementById("dob").style.border = "4px solid #ff3333";
+        document.getElementById("edob").style.display = "inline-block";
+        error = true;
+    } else {
+        document.getElementById("dob").style.border = "4px solid rgb(21, 221, 21)";
+        document.getElementById("edob").style.display = "none";
+    }
+
+    if (gpa === "" || !(/[+-]?([0-9]*[.])?[0-9]/.test(gpa)) || (to_float(gpa) > 4.0 || to_float(gpa) < 0.0)) {
+        document.getElementById("gpa").style.border = "4px solid #ff3333";
+        document.getElementById("egpa").style.display = "inline-block";
+        error = true;
+    } else {
+        document.getElementById("gpa").style.border = "4px solid rgb(21, 221, 21)";
+        document.getElementById("egpa").style.display = "none";
+    }
+
+    if(email !== ""){
+        if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+            document.getElementById("email").style.border = "4px solid #ff3333";
+            document.getElementById("eemail").style.display = "inline-block";
+            error = true;
+        } else {
+            document.getElementById("email").style.border = "4px solid rgb(21, 221, 21)";
+            document.getElementById("eemail").style.display = "none";
+        }
+    }
+    
+    if(mobile !== ""){
+        if(!(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(mobile))){
+            document.getElementById("mobile").style.border = "4px solid #ff3333";
+            document.getElementById("emobile").style.display = "inline-block";
+            error = true;
+        } else {
+            document.getElementById("mobile").style.border = "4px solid rgb(21, 221, 21)";
+            document.getElementById("emobile").style.display = "none";
+        }
+    }
+
+    if (female === false && male === false) {
+        document.getElementById("male").style.outline = "4px solid #ff3333";
+        document.getElementById("female").style.outline = "4px solid #ff3333";
+        document.getElementById("egender").style.display = "inline-block";
+        error = true;
+    } else {
+        document.getElementById("male").style.outline = "4px solid rgb(21, 221, 21)";
+        document.getElementById("female").style.outline = "4px solid rgb(21, 221, 21)";
+        document.getElementById("egender").style.display = "none";
+    }
+
+    if (active === false && inactive === false) {
+        document.getElementById("active").style.outline = "4px solid #ff3333";
+        document.getElementById("inactive").style.outline = "4px solid #ff3333";
+        document.getElementById("estatus").style.display = "inline-block";
+        error = true;
+    } else {
+        document.getElementById("active").style.outline = "4px solid rgb(21, 221, 21)";
+        document.getElementById("inactive").style.outline = "4px solid rgb(21, 221, 21)";
+        document.getElementById("estatus").style.display = "none";
+    }
+
+    let is_lv_valid = (lv === "" || !(/[+-]?([0-9]*[.])?[0-9]/.test(lv)) || to_float(lv)%1 !== 0 || (to_float(lv) > 4.0 || to_float(lv) < 0.0));
+    if (is_lv_valid) {
+        document.getElementById("lv").style.border = "4px solid #ff3333";
+        document.getElementById("elv").style.display = "inline-block";
+        error = true;
+    } else {
+        document.getElementById("lv").style.border = "4px solid rgb(21, 221, 21)";
+        document.getElementById("elv").style.display = "none";
+    }
+
+    if(!error){
+        document.getElementById("edit-form").submit();
+    }
+}
 
 function register(){
     error = false
@@ -52,8 +161,8 @@ function register(){
     let female = document.getElementById("female").checked;
     let mobile = document.getElementById("mobile").value;
     let email = document.getElementById("email").value;
-    let active = document.getElementById("active").value;
-    let inactive = document.getElementById("inactive").value;
+    let active = document.getElementById("active").checked;
+    let inactive = document.getElementById("inactive").checked;
     let dept = document.getElementById("dept").innerText;
     
     if (id === "" || id.length > 8 || !(/^[0-9]*$/.test(id))) {
@@ -123,6 +232,17 @@ function register(){
         document.getElementById("male").style.outline = "4px solid rgb(21, 221, 21)";
         document.getElementById("female").style.outline = "4px solid rgb(21, 221, 21)";
         document.getElementById("egender").style.display = "none";
+    }
+
+    if (active === false && inactive === false) {
+        document.getElementById("active").style.outline = "4px solid #ff3333";
+        document.getElementById("inactive").style.outline = "4px solid #ff3333";
+        document.getElementById("estatus").style.display = "inline-block";
+        error = true;
+    } else {
+        document.getElementById("active").style.outline = "4px solid rgb(21, 221, 21)";
+        document.getElementById("inactive").style.outline = "4px solid rgb(21, 221, 21)";
+        document.getElementById("estatus").style.display = "none";
     }
 
     let is_lv_valid = (lv === "" || !(/[+-]?([0-9]*[.])?[0-9]/.test(lv)) || to_float(lv)%1 !== 0 || (to_float(lv) > 4.0 || to_float(lv) < 0.0));
